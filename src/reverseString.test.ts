d55d2a5b124fe5630cf1128b6f2805d843a86be1
Egor Mesyats <egor.mesyats@fiverr.com>
2022-02-20 18:45:27 +0200

    Revert "test: failed case"
    This reverts commit def5ce68574044aa295c6022b01050431c083d0e.

--- src/reverseString.test.ts
+++ src/reverseString.test.ts
@@ -24,7 +24,4 @@ describe('reverseString', () => {
   it('string case is preserved', () => {
     test('HelLo', 'oLleH')
   })
-  it('wrong test', () => {
-    test('this is the input', 'WRONG RESULT')
-  })
 })
