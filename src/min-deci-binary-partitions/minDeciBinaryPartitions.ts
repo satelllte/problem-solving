fb53af33a6621995f3f0a0e90c124569b4f69f09
George <ast809809@gmail.com>
2022-06-28 12:10:18 +0000

    solution

--- src/min-deci-binary-partitions/minDeciBinaryPartitions.ts
+++ src/min-deci-binary-partitions/minDeciBinaryPartitions.ts
@@ -1,3 +1,17 @@
 export const minDeciBinaryPartitions = (n: string): number => {
-  return 0 // TODO: solve
+  let max = -Infinity
+
+  const nums = n.split('')
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = Number(nums[i])
+    if (num > max) {
+      max = num
+    }
+    if (max >= 9) {
+      return max
+    }
+  }
+
+  return max
 }
