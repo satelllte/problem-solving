1bb3bef8e0469027ba268c8394e9681322f7cfff
George <ast809809@gmail.com>
2022-06-29 15:28:04 +0000

    unit tests

--- src/problems/leetcode/0017-letter-combinations-of-a-phone-number/letterCombinations.test.ts
+++ src/problems/leetcode/0017-letter-combinations-of-a-phone-number/letterCombinations.test.ts
@@ -1,9 +1,28 @@
 import { letterCombinations } from './letterCombinations'
 
 describe('letterCombinations', () => {
-  it('works', () => {
-    expect(letterCombinations('23')).toEqual(['ad','ae','af','bd','be','bf','cd','ce','cf'])
+  it('empty input has no output', () => {
     expect(letterCombinations('')).toEqual([])
-    expect(letterCombinations('2')).toEqual(['a','b','c'])
+  })
+  
+  it('"1" does not affect output', () => {
+    expect(letterCombinations('1')).toEqual([])
+    expect(letterCombinations('11')).toEqual([])
+    expect(letterCombinations('12')).toEqual(['a','b','c'])
+    expect(letterCombinations('31')).toEqual(['d','e','f'])
+  })
+
+  it('combinations work', () => {
+    expect(letterCombinations('23')).toEqual([
+      'ad','ae','af',
+      'bd','be','bf',
+      'cd','ce','cf',
+    ])
+    expect(letterCombinations('78')).toEqual([
+      'pt','pu','pv',
+      'qt','qu','qv',
+      'rt','ru','rv',
+      'st','su','sv',
+    ])
   })
 })
