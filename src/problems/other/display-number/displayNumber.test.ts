b611ef1da27f2bf735769d1a0a37bdb9ec63babc
George <ast809809@gmail.com>
2022-07-06 15:15:58 +0000

    switch to `vitest` from `jest`

--- src/problems/other/display-number/displayNumber.test.ts
+++ src/problems/other/display-number/displayNumber.test.ts
@@ -1,14 +1,15 @@
+import { describe, test, expect } from 'vitest'
 import { displayNumber } from './displayNumber'
 
 describe('displayNumber', () => {
-  it('before thousands, leaves the result as is', () => {
+  test('before thousands, leaves the result as is', () => {
     expect(displayNumber(1)).toEqual('1')
     expect(displayNumber(10)).toEqual('10')
     expect(displayNumber(100)).toEqual('100')
     expect(displayNumber(999)).toEqual('999')
   })
 
-  it('separates thousands', () => {
+  test('separates thousands', () => {
     expect(displayNumber(1_000)).toEqual('1 000')
     expect(displayNumber(10_000)).toEqual('10 000')
     expect(displayNumber(100_000)).toEqual('100 000')
